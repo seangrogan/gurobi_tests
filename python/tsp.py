8314bf4e4a7e6c0d87f58b9352566a419bbbeacb
Sean Grogan <sean.grogan@gmail.com>
2018-12-12 15:28:53 -0500

    Updated TSP to include MatPlotLib to display the points and the tour.

--- python/tsp.py
+++ python/tsp.py
@@ -9,11 +9,11 @@
 # tours that don't visit every city.  The lazy constraint callback
 # adds new constraints to cut them off.
 
-import sys
-import math
 import random
-import itertools
+
+import matplotlib.pyplot as plt
 from gurobipy import *
+
 
 # Callback - use lazy constraints to eliminate sub-tours
 
@@ -21,29 +21,29 @@ def subtourelim(model, where):
     if where == GRB.Callback.MIPSOL:
         # make a list of edges selected in the solution
         vals = model.cbGetSolution(model._vars)
-        selected = tuplelist((i,j) for i,j in model._vars.keys() if vals[i,j] > 0.5)
+        selected = tuplelist((i, j) for i, j in model._vars.keys() if vals[i, j] > 0.5)
         # find the shortest cycle in the selected edge list
         tour = subtour(selected)
         if len(tour) < n:
             # add subtour elimination constraint for every pair of cities in tour
-            model.cbLazy(quicksum(model._vars[i,j]
-                                  for i,j in itertools.combinations(tour, 2))
-                         <= len(tour)-1)
+            model.cbLazy(quicksum(model._vars[i, j]
+                                  for i, j in itertools.combinations(tour, 2))
+                         <= len(tour) - 1)
 
 
 # Given a tuplelist of edges, find the shortest subtour
 
 def subtour(edges):
     unvisited = list(range(n))
-    cycle = range(n+1) # initial length has 1 more city
-    while unvisited: # true if list is non-empty
+    cycle = range(n + 1)  # initial length has 1 more city
+    while unvisited:  # true if list is non-empty
         thiscycle = []
         neighbors = unvisited
         while neighbors:
             current = neighbors[0]
             thiscycle.append(current)
             unvisited.remove(current)
-            neighbors = [j for i,j in edges.select(current,'*') if j in unvisited]
+            neighbors = [j for i, j in edges.select(current, '*') if j in unvisited]
         if len(cycle) > len(thiscycle):
             cycle = thiscycle
     return cycle
@@ -59,21 +59,23 @@ n = int(sys.argv[1])
 # Create n random points
 
 random.seed(1)
-points = [(random.randint(0,100),random.randint(0,100)) for i in range(n)]
-
+points = [(random.randint(0, 100), random.randint(0, 100)) for i in range(n)]
+x, y = zip(*points)
+plt.plot(x, y, 'o')
+plt.show()
 # Dictionary of Euclidean distance between each pair of points
 
-dist = {(i,j) :
-    math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
-    for i in range(n) for j in range(i)}
+dist = {(i, j):
+            math.sqrt(sum((points[i][k] - points[j][k]) ** 2 for k in range(2)))
+        for i in range(n) for j in range(i)}
 
 m = Model()
 
 # Create variables
 
 vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
-for i,j in vars.keys():
-    vars[j,i] = vars[i,j] # edge in opposite direction
+for i, j in vars.keys():
+    vars[j, i] = vars[i, j]  # edge in opposite direction
 
 # You could use Python looping constructs and m.addVar() to create
 # these decision variables instead.  The following would be equivalent
@@ -87,7 +89,7 @@ for i,j in vars.keys():
 
 # Add degree-2 constraint
 
-m.addConstrs(vars.sum(i,'*') == 2 for i in range(n))
+m.addConstrs(vars.sum(i, '*') == 2 for i in range(n))
 
 # Using Python looping constructs, the preceding would be...
 #
@@ -102,10 +104,16 @@ m.Params.lazyConstraints = 1
 m.optimize(subtourelim)
 
 vals = m.getAttr('x', vars)
-selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.5)
+selected = tuplelist((i, j) for i, j in vals.keys() if vals[i, j] > 0.5)
 
 tour = subtour(selected)
 assert len(tour) == n
+
+x, y = [points[customer][0] for customer in tour], [points[customer][1] for customer in tour]
+x.append(points[tour[0]][0])
+y.append(points[tour[0]][1])
+plt.plot(x, y, 'r-')
+plt.show()
 
 print('')
 print('Optimal tour: %s' % str(tour))
